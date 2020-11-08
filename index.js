52987d1864eb60ab6c3e85ab203bee109efb3863
sepehr kashani <SepehrKashani@sepehrs-MacBook-Pro.local>
2020-11-08 11:37:21 +1100

    My second commit, getting date and list working

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 const inpt = document.querySelector('.todoInput');
-const inptValue = document.querySelector('.todoInput').value;
-const listItems = document.querySelector('.listItems');
+// const inptValue = document.querySelector('.todoInput').value;
+const ulList = document.querySelector('.listItems');
+const date = document.querySelector('#heading-date');
+const day = document.querySelector('#subheading-day');
+
+(function () {
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  Date.prototype.getMonthName = function () {
+    return months[this.getMonth()];
+  };
+  Date.prototype.getDayName = function () {
+    return days[this.getDay()];
+  };
+})();
+
+const now = new Date();
+
+day.textContent = now.getDayName();
+date.textContent = `${now.getMonthName()}, ${now.getDate()}, ${now.getFullYear()}`;
 
 const list = function () {
-  const todoList = document.createElement('ul');
-
   const todoItem = document.createElement('li');
-
   todoItem.textContent = inpt.value;
-
-  todoList.appendChild(todoItem);
-  listItems.appendChild(todoList);
-  //   document.body.appendChild(todoList);
+  // ulList.classList.add('undone');
+  // todoItem.classList.add('undone');
+  ulList.appendChild(todoItem);
+  ulList.classList.add('undone');
+  inpt.value = '';
 };
 
 const handleEvent = function (e) {
@@ -26,4 +65,14 @@ const handleEvent = function (e) {
 // btn.addEventListener('click', handleEvent);
 // inpt.addEventListener('input', handleEvent);
 
+const handleList = function (e) {
+  const targetEvent = e.target;
+  // console.log('you prssed togle');
+  console.log(e.target);
+  console.log(e.currentTarget);
+
+  targetEvent.classList.toggle('done');
+};
+
 inpt.addEventListener('keypress', handleEvent);
+ulList.addEventListener('click', handleList);
